21e45bc4e914fc2c64adcd4c6ac9594bbc7c5ba3
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-04-18 10:05:48 +0800

    add test and pipeline2

---
+++ auto/test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./mvnw clean
+./mvnw test
--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,26 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+	@Autowired
+	lateinit var mockMvc: MockMvc
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	internal fun `should return 200`() {
+		mockMvc.perform(get("/")).andExpect(status().isOk)
+	}
 }
